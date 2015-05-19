e64ac1ad4aa659f3ead17987a50eef7660e5e427
Paco Gonzálvez <mad8bits@gmail.com>
2015-05-19 12:57:34 +0200

    Update RandomVideogames.js

--- RandomVideogames.js
+++ RandomVideogames.js
@@ -1,17 +1,10 @@
 function searchGame() {
-
-var numberSelected = Math.floor((Math.random() * videoGameList.length) + 1)
-
-var gameSelected = videoGameList[numberSelected]
-
-gameSelected.scrollIntoView( true )
-
-gameSelected.style.backgroundColor="red"
-
+  var numberSelected = Math.floor((Math.random() * videoGameList.length) + 1);
+  var gameSelected = videoGameList[numberSelected];
+  gameSelected.scrollIntoView(true);
+  gameSelected.style.backgroundColor = "red";
 }
 
-var rowInserted = $('div table').insertRow()
-
-rowInserted.innerHTML = "<button onclick='searchGame()'> Get Random Game </button>"
-
-var videoGameList = $(".games").getElementsByTagName('a')
+var rowInserted = $('div table').insertRow();
+rowInserted.innerHTML = "<button onclick='searchGame()'> Get Random Game </button>";
+var videoGameList = $(".games").getElementsByTagName('a');
